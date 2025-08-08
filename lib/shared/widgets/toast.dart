cef4edcc8a56fed335dbcfc35795dbb0d8e8f094
민경 <tjalsrud0101@gmail.com>
2025-08-08 17:10:12 +0900

    fix: text overflow 처리, 위치 조정(#127)

--- lib/shared/widgets/toast.dart
+++ lib/shared/widgets/toast.dart
@@ -10,27 +10,44 @@ import 'package:ormee_app/shared/theme/app_fonts.dart';
 class OrmeeToast {
   static void show(BuildContext context, String message, bool isError) {
     final overlay = Overlay.of(context);
+    final maxWidth = MediaQuery.of(context).size.width * 0.9;
+
     final overlayEntry = OverlayEntry(
       builder: (context) => Positioned(
-        top: MediaQuery.of(context).size.height * 0.1, // 화면 하단 20% 위치
-        left: 20,
-        right: 20,
+        top: MediaQuery.of(context).size.height * 0.1,
+        left: 0,
+        right: 0,
         child: Material(
           color: Colors.transparent,
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-            decoration: BoxDecoration(
-              color: isError ? OrmeeColor.systemRed : OrmeeColor.systemGreen,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Row(
-              children: [
-                SvgPicture.asset(isError? 'assets/icons/alert.svg' : 'assets/icons/complete.svg'),
-                Headline1SemiBold18(
-                  text: message,
-                  color: OrmeeColor.gray[90],
+          child: Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: maxWidth,
+              ),
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                decoration: BoxDecoration(
+                  color: isError ? OrmeeColor.systemRed : OrmeeColor.systemGreen,
+                  borderRadius: BorderRadius.circular(10),
                 ),
-              ],
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      isError ? 'assets/icons/alert.svg' : 'assets/icons/complete.svg',
+                    ),
+                    SizedBox(width: 8),
+                    Flexible(
+                      child: Headline1SemiBold18(
+                        text: message,
+                        color: OrmeeColor.gray[90],
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
@@ -38,8 +55,6 @@ class OrmeeToast {
     );
 
     overlay.insert(overlayEntry);
-
-    // 일정 시간 후 제거
     Future.delayed(Duration(seconds: 2)).then((_) => overlayEntry.remove());
   }
 }
