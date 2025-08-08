57e0f90e0e7f8125d2eb651a49578d4dbc570ef3
민경 <tjalsrud0101@gmail.com>
2025-08-08 12:08:27 +0900

    chore: 패키지명 변경(#143)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 }
 
 android {
-    namespace = "com.example.ormee_app"
+    namespace = "com.ormee.app"
     compileSdk = flutter.compileSdkVersion
     // ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -37,7 +37,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ormee_app"
+        applicationId = "com.ormee.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
