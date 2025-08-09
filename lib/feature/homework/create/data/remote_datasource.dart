5b9bb99859c11b8f2589da544cb1e98aa3190a6d
민경 <tjalsrud0101@gmail.com>
2025-08-10 02:39:34 +0900

    fix: 이미지 저장 시 토큰 누락 에러(#147)

--- lib/feature/homework/create/data/remote_datasource.dart
+++ lib/feature/homework/create/data/remote_datasource.dart
@@ -1,24 +1,24 @@
 import 'package:dio/dio.dart';
-import 'package:http/http.dart' as http;
 import 'package:ormee_app/core/network/api_client.dart';
 import 'package:ormee_app/feature/homework/create/data/model.dart';
 
 class HomeworkCreateRemoteDataSource {
-  final http.Client client;
-
-  HomeworkCreateRemoteDataSource(this.client);
-
   final Dio _dio = ApiClient.instance.dio;
 
   Future<void> postHomework(int homeworkId, HomeworkRequest request) async {
-    final response = await _dio.post(
-      '/students/homeworks/$homeworkId',
-      data: request,
-      options: Options(headers: {'Content-Type': 'application/json'}),
-    );
+    try {
+      final response = await _dio.post(
+        '/students/homeworks/$homeworkId',
+        data: request.toJson(),
+      );
 
-    if (response.statusCode != 200 && response.statusCode != 201) {
-      throw Exception('숙제 등록 실패: ${response.data}');
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return;
+      } else {
+        throw Exception('숙제를 등록할 수 없습니다.');
+      }
+    } catch (e) {
+      throw Exception('숙제 등록 중 오류가 발생했습니다.');
     }
   }
 }
