7fc303a9751e7149e8fe45d8c832ddeabb66f74e
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-08-18 13:30:24 +0900

    fix: 비밀번호 찾기 디자인 변경 (#166)

--- lib/feature/auth/find/presentation/widgets/password_tab.dart
+++ lib/feature/auth/find/presentation/widgets/password_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ormee_app/shared/theme/app_fonts.dart';
 import 'package:ormee_app/shared/widgets/bottomsheet.dart';
+import 'package:ormee_app/shared/widgets/button.dart';
 
 class PasswordTab extends StatelessWidget {
   const PasswordTab({super.key});
@@ -9,19 +10,30 @@ class PasswordTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Body2SemiBoldNormal14(text: "비밀번호를 찾으려면 본인인증이 필요해요."),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Body2RegularNormal14(text: "비밀번호를 찾으려면 본인인증이 필요해요."),
+            SizedBox(height: 16),
+            OrmeeButton(
+              text: '본인인증 하기',
+              isTrue: true,
+              // trueAction: () {
+              //   // 추후 본인 인증 연결
+              //   context.push(
+              //     '/find/password',
+              //     extra: {
+              //       'username': "student3",
+              //       'phoneNumber': "010-4444-4444",
+              //     },
+              //   );
+              // },
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+          ],
+        ),
       ),
-      // bottomSheet: OrmeeBottomSheet(
-      //   text: "본인인증 하기",
-      //   isCheck: true,
-      //   // 추후 본인인증 연결
-      //   onTap: () => context.push(
-      //     '/find/password',
-      //     extra: {'username': "student3", 'phoneNumber': "010-4444-4444"},
-      //   ),
-      // ),
-      bottomSheet: OrmeeBottomSheet(text: '본인인증 하기', isCheck: false),
     );
   }
 }
